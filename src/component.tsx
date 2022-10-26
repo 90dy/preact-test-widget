1d8e77c7099cc25b392596f8f10dd6e7d32138d8
alexandre bertrand <a-bertrand@autodistribution.com>
2022-10-26 14:57:26 +0200

    init

---
+++ src/component.tsx
@@ -0,0 +1,23 @@
+import { h } from "preact";
+import { FC, useCallback } from "react";
+import { useForm, FormProvider, useController } from "react-hook-form";
+import "./style.css";
+
+interface Props {
+  color?: string;
+}
+
+export const App: FC<Props> = (props): JSX.Element => {
+  const formMethods = useForm({
+    mode: "onChange",
+  });
+  return (
+    <FormProvider {...formMethods}>
+      <form>
+        <h1 style={{ color: props.color }}>Hello, World!</h1>
+      </form>
+    </FormProvider>
+  );
+};
+
+export default App;
