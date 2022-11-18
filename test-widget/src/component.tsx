de5c63e743bc71325deb15fb66464754d4a18909
90dy <90dy@proton.me>
2022-11-18 14:48:38 +0100

    fix(yolo): :muscle:
    Signed-off-by: 90dy <90dy@proton.me>

--- test-widget/src/component.tsx
+++ test-widget/src/component.tsx
@@ -1,24 +1,27 @@
 import { h, VNode } from "preact";
-import { useForm, FormProvider } from "react-hook-form";
+import { useEffect } from "preact/hooks";
+// import { useForm, FormProvider } from "react-hook-form";
 import "./style.css";
 
 interface Props {
   color?: string;
 }
 
-const App = (props: Props): VNode => {
-  console.log("helloo");
-
-  const formMethods = useForm({
-    mode: "onChange",
-  });
-  return (
-    <FormProvider {...formMethods}>
-      <form>
-        <h1 style={{ color: props.color }}>Hello, World!</h1>
-      </form>
-    </FormProvider>
-  );
+export const App = (props: Props): VNode => {
+  useEffect(() => {
+    console.log("helloo");
+  }, []);
+  return <div>preact</div>;
+  // const formMethods = useForm({
+  //   mode: "onChange",
+  // });
+  // return (
+  //   <FormProvider {...formMethods}>
+  //     <form>
+  //       <h1 style={{ color: props.color }}>Hello, World!</h1>
+  //     </form>
+  //   </FormProvider>
+  // );
 };
 
 export default App;
